89ad6c31c49aba0f4ae5c4423925e7521143d264
wec410sin <wec410sin@yahoo.co.jp>
2021-03-11 21:20:49 +0900

    main.rbのrequireの修正

--- main.rb
+++ main.rb
@@ -1,10 +1,10 @@
+require './methods.rb'
+
 plans = [
   { place: "沖縄", price: 10000},
   { place: "北海道", price: 20000},
   { place: "九州", price: 15000}
 ]
-
-require('./methods.rb')
 
 disp_plans(plans)
 chosen_plan = choose_plan(plans)
